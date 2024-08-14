549cd7a2128c6b0c1bd029db6e0a630f66d5427d
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-08-14 13:37:26 +0300

    Add Skills to main page.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,7 @@ import About from './sections/About';
 import Contact from './sections/Contact';
 import Introduction from './sections/Introduction';
 import Projects from './sections/Projects';
+import Skills from './sections/Skills';
 import WorkExperience from './sections/WorkExperience';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <TopBar />
             <Introduction />
             <About />
+            <Skills />
             <Projects />
             <WorkExperience />
             <Contact />
