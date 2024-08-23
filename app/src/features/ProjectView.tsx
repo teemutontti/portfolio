aacd7f4de55cc2d9d3ffe6725919a407740443b4
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-08-23 16:26:17 +0300

    Add feature for viewing single project.

---
+++ app/src/features/ProjectView.tsx
@@ -0,0 +1,43 @@
+import { ProjectViewType } from "../util/types";
+import placeholderImage from "../assets/placeholder.webp";
+import "../styles/project-view.css";
+import Logo from "../components/Logo";
+
+export default function ProjectView({ project, onClose }: ProjectViewType) {
+    return (
+        <div className="project-view">
+            <div className="panel">
+                <button className="close" onClick={onClose}>
+                    <span className="material-symbols-outlined">close</span>
+                </button>
+                <div>
+                    <h3>{project.name}</h3>
+                    <p className="subtitle">{project.date}</p>
+                </div>
+                <div>
+                    <img
+                        src={project.image || placeholderImage}
+                        alt="Project image"
+                        className="project-image"
+                    />
+                </div>
+                <div>
+                    <h4>Description</h4>
+                    <div className="description">
+                        {project.description.map((paragraph, index) => (
+                            <p key={index}>{paragraph}</p>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h4>Technologies used</h4>
+                    <div className="logos">
+                        {project.logos.map((logo, index) => (
+                            <Logo name={logo} key={index} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
