26a4594cdb109f4f8e4c8b4376e81936c0776c24
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-08-26 23:36:36 +0300

    Use ProjectView on project click.

--- app/src/sections/Projects.tsx
+++ app/src/sections/Projects.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import ProjectCard from "../components/ProjectCard";
 import "../styles/projects.css";
+import projectObjArray from "../util/projects";
+import { ProjectType } from "../util/types";
+import ProjectView from "../features/ProjectView";
 
 export default function Projects() {
+    const [projectToView, setProjectToView] = useState<ProjectType | null>(null);
+
+    const handleProjectClick = (project: ProjectType) => {
+        setProjectToView(project);
+    };
+
     return (
         <section className="projects" id="projects">
-            <h2>Projects</h2>
-            <p>This is a simple React app that demonstrates how to use React components, props, and state.</p>
+            <h2>Recent Projects</h2>
+            {projectToView &&
+                <ProjectView
+                    project={projectToView}
+                    onClose={() => setProjectToView(null)}
+                />
+            }
+            <div className="project-list">
+                {projectObjArray.map((project, index) => (
+                    <ProjectCard
+                        key={index}
+                        project={project}
+                        onClick={() => handleProjectClick(project)}
+                    />
+                ))}
+            </div>
         </section>
     );
 }
