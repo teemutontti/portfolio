59e8d5217ca6517c7f3263c10cf751febd5a7a7d
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-08-27 01:55:09 +0300

    Use loading spinner.

--- app/src/features/ProjectView.tsx
+++ app/src/features/ProjectView.tsx
@@ -2,8 +2,26 @@ import { ProjectViewType } from "../util/types";
 import placeholderImage from "../assets/placeholder.webp";
 import "../styles/project-view.css";
 import Logo from "../components/Logo";
+import { useEffect, useState } from "react";
+import LoadingEffect from "../components/LoadingEffect";
 
 export default function ProjectView({ project, onClose }: ProjectViewType) {
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const img = new Image();
+
+        if (!project.image) {
+            return;
+        }
+
+        img.src = project.image;
+        img.onload = () => {
+            setLoading(false);
+        };
+
+    }, [project.image]);
+
     return (
         <div className="project-view">
             <div className="panel">
@@ -15,11 +33,14 @@ export default function ProjectView({ project, onClose }: ProjectViewType) {
                     <p className="subtitle">{project.date}</p>
                 </div>
                 <div>
-                    <img
-                        src={project.image || placeholderImage}
-                        alt="Project image"
-                        className="project-image"
-                    />
+                    {loading
+                        ? <LoadingEffect />
+                        : <img
+                            src={project.image || placeholderImage}
+                            alt="Project image"
+                            className="project-image"
+                        />
+                    }
                 </div>
                 <div>
                     <h4>Description</h4>
