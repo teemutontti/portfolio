b72af0a6dd5f8dee2dfeac4ae3b6ca261ec4fc2a
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-08-26 23:42:01 +0300

    Add new skill tags and use LogoDumb.

--- app/src/sections/Skills.tsx
+++ app/src/sections/Skills.tsx
@@ -1,14 +1,18 @@
-import Logo from "../components/Logo";
+//import Logo from "../components/Logo";
 import LogoDumb from "../components/LogoDumb";
-import PercentageWheel from "../components/PercentageWheel";
+//import PercentageWheel from "../components/PercentageWheel";
 import Tag from "../components/Tag";
 import "../styles/skills.css"
 
 export default function Skills() {
     return (
-        <section className="skills">
-            <h2>Skills</h2>
-            <LogoDumb />
+        <section className="skills" id="skills">
+            <p>
+                Here's a snapshot of the technologies and methodologies I'm proficient in, with a focus on full-stack development
+                and mobile applications.
+            </p>
+            <LogoDumb version={2} />
+            {/*
             <div className="logo-list">
                 <div className="wheel">
                     <PercentageWheel percentage={80} />
@@ -23,6 +27,8 @@ export default function Skills() {
                     <p>Design</p>
                 </div>
             </div>
+            */}
+            <p>Hover over the tags to show where I have used each skill</p>
             <div className="logo-list">
                 <Tag>Frontend</Tag>
                 <Tag>Backend</Tag>
@@ -30,24 +36,14 @@ export default function Skills() {
                 <Tag>Problem Solving</Tag>
                 <Tag>Self-Initiative</Tag>
                 <Tag>Teamwork</Tag>
+                {/* Not sure if worth noting
                 <Tag>TDD</Tag>
+                */}
                 <Tag>Critical Thinking</Tag>
+                <Tag>Industrial Protocols</Tag>
+                <Tag>Game Development</Tag>
+                <Tag>Databases</Tag>
                 <Tag>Internship Experience</Tag>
-            </div>
-            <div className="logo-list">
-                <Logo name="html" />
-                <Logo name="css" />
-                <Logo name="js" />
-                <Logo name="ts" />
-                <Logo name="java" />
-                <Logo name="python" />
-                <Logo name="android" />
-                <Logo name="kotlin" />
-                <Logo name="mysql" />
-                <Logo name="react" />
-                <Logo name="spring" />
-                <Logo name="node" />
-                <Logo name="express" />
             </div>
         </section>
     );
