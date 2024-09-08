6e692440578ee70a9fc63bb5f159f7d2de188601
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-09-08 18:44:28 +0300

    Move build files to root.

---
+++ assets/About-CHRgHESd.js
@@ -0,0 +1,3 @@
+import{u as e,j as t}from"./index-D1PQW6GY.js";import{S as o}from"./Socials-BR7Z9OSK.js";import{S as n}from"./Section-B23KeccM.js";import"./linkedin-app-white-icon-CE_SIj6y.js";import"./Icon-DVh2qAaY.js";function x(){const{t:s}=e();return t.jsxs(n,{name:"about",title:s("about.title"),children:[t.jsx("div",{className:"text-field",children:s("about.text").split(`
+`).map((i,a)=>t.jsx("p",{children:i},a))}),t.jsxs("div",{className:"contact",id:"contact",children:[t.jsx("div",{className:"text",children:s("contact").split(`
+`).map((i,a)=>t.jsx("p",{children:i},a))}),t.jsx(o,{})]})]})}export{x as default};
