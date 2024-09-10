994ad729c18dc7d11df88da096134202105e383b
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-09-10 20:08:58 +0300

    Add outer links to project view.

--- app/src/features/ProjectView.tsx
+++ app/src/features/ProjectView.tsx
@@ -28,10 +28,6 @@ export default function ProjectView({ project, onClose }: ProjectViewType) {
 
     }, [project.thumbnail]);
 
-    const handleImageClick = (index: number) => {
-        setSelectedImage(index);
-    }
-
     return (
         <div className="project-view">
             <div className="panel">
@@ -53,14 +49,43 @@ export default function ProjectView({ project, onClose }: ProjectViewType) {
                                 className="project-image"
                                 loading="lazy"
                             />
+                            <div className="outer-links">
+                                {project.mainLink && (
+                                    <a href={project.mainLink} target="_blank" rel="noreferrer" className="main-link">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                                            <path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                                        </svg>
+                                        {t("projects.livePreview")}
+                                    </a>
+                                )}
+                                {project.secondaryLink && (
+                                    <a href={project.secondaryLink} target="_blank" rel="noreferrer" className="secondary-link">
+                                        <Logo name="github" clickable={false} />
+                                    </a>
+                                )}
+                            </div>
+                            {/*
+                            <div className="image-change-buttons">
+                                <button onClick={() => handleImageChange(-1)}>
+                                    <Icon name="chevron-left" />
+                                </button>
+                                <span className="image-number">
+                                    {selectedImage + 1} / {project.images.length}
+                                </span>
+                                <button onClick={() => handleImageChange(1)}>
+                                    <Icon name="chevron-right" />
+                                </button>
+                            </div>
+                            */}
                             <div className="image-gallery">
                                 {project.images.map((image, index) => (
-                                    <button
-                                        onClick={() => handleImageClick(index)}
-                                        className={selectedImage === index ? "img-button selected" : "img-button"}
-                                        key={index}
-                                    >
-                                        <img src={image} alt="Project image" loading="lazy" />
+                                    <button key={index} className={selectedImage === index ? "img-button selected" : "img-button"}>
+                                        <img
+                                            src={image}
+                                            alt="Project image"
+                                            key={index}
+                                            onClick={() => setSelectedImage(index)}
+                                        />
                                     </button>
                                 ))}
                             </div>
