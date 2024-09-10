ad75b6e0accd0aef6406eac08597532aefc864c6
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-09-10 20:12:35 +0300

    Move build files to root.

---
+++ assets/About-CUzKAIRS.js
@@ -0,0 +1,3 @@
+import{u as e,j as t}from"./index-BEcISmhI.js";import{S as o}from"./Socials-D2CNUI6R.js";import{S as n}from"./Section-B1x-Qsre.js";import"./linkedin-app-white-icon-CE_SIj6y.js";import"./Icon-BABPZ79U.js";function x(){const{t:s}=e();return t.jsxs(n,{name:"about",title:s("about.title"),children:[t.jsx("div",{className:"text-field",children:s("about.text").split(`
+`).map((i,a)=>t.jsx("p",{children:i},a))}),t.jsxs("div",{className:"contact",id:"contact",children:[t.jsx("div",{className:"text",children:s("contact").split(`
+`).map((i,a)=>t.jsx("p",{children:i},a))}),t.jsx(o,{})]})]})}export{x as default};
---
+++ assets/Section-B1x-Qsre.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BEcISmhI.js";function t({name:e,title:n,children:c}){return s.jsxs("section",{className:`styled-section ${e}`,id:e,children:[n&&s.jsx("h2",{className:"heading",children:n}),s.jsx("div",{className:"content",children:c})]})}export{t as S};
