78f99e021e1c04855038246c4dc2ef8129a65b55
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-03-28 20:02:11 +0200

    Add sublink to split the gas

---
+++ app/src/pages/SplitTheGas.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function SplitTheGas() {
+    useEffect(() => {
+        window.location.href = "https://blue-bush-0a7fe1203.6.azurestaticapps.net/"
+    }, [])
+
+    return null;
+}
