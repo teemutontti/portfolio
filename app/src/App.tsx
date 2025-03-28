e3b48b51f8486dd9b85a112b90d54243a871cbd2
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-03-28 20:03:13 +0200

    Add sublink to split the gas

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import SplitTheGas from './pages/SplitTheGas';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/split-the-gas" element={<SplitTheGas />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
