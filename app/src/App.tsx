41c390544b9f75d9ca4d0b683a3f296f36512dda
Teemu Tontti <teemu.tontti.tt@gmail.com>
2025-03-28 20:10:38 +0200

    fix: basename bug

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,7 +6,7 @@ import SplitTheGas from './pages/SplitTheGas';
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/split-the-gas" element={<SplitTheGas />} />
